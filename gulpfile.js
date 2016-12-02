dff99a944bbcff44e088fd005543811b26f97683
Osman Ramazanov <osman.ramazanov@gmail.com>
2016-12-02 21:02:08 +0200

    init

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp'),
+	connect = require('gulp-connect'),
+  less = require('gulp-less');
+var includer = require('gulp-htmlincluder');
+
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'build',
+    livereload: true
+  });
+});
+
+gulp.task('less', function () {
+  return gulp.src('dev/less/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('build/css'))
+    .pipe(connect.reload());
+});
+
+    connect = require('gulp-connect');
+
+gulp.task('minify-css', function() {
+    return gulp.src('dev/css/*.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('build/css/'));
+});
+
+gulp.task('html', function() {
+    gulp.src('dev/**/*.html')
+        .pipe(includer())
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('move', function(){
+ gulp.src('dev/**/*.html').pipe(gulp.dest('build/'));
+ connect.reload();
+});
+
+gulp.task('default', function(){
+  gulp.start('connect', 'less', 'html');
+  gulp.watch(['dev/less/**/**/*.less'], ['less']);
+  gulp.watch(['dev/**/**/*.html'], ['html']);
+});
